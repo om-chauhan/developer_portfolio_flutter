32728d9af8bbf71bf24379d694ba9ed2be4b30ae
Omprakash Chauhan <omi8655@gmail.com>
2022-06-13 22:02:09 +0530

    project details updated

--- lib/controller/project_controller.dart
+++ lib/controller/project_controller.dart
@@ -3,40 +3,52 @@ import 'package:om_chauhan/all_imports.dart';
 class ProjectController {
   final projectList = [
     ProjectModel(
-        title: 'Project Name',
+        title: 'Flutter Snippets',
         description:
-            "is simply dummy text of the printing and tyesetting industry. Lorem Ipsum has been the industry's standard.",
-        website: 'http://google.com',
-        gitHub: 'http://github.com'),
+            "Flutter Snippets is a collection of mostly used Widgets and Classes for Flutter developers.",
+        website:
+            'https://marketplace.visualstudio.com/items?itemName=om-chauhan.flutter-snippets',
+        gitHub: 'https://github.com/om-chauhan/flutter-snippets'),
     ProjectModel(
-        title: 'Project Name',
+        title: 'Create flutter package',
         description:
-            "is simply dummy text of the printing and tyesetting industry. Lorem Ipsum has been the industry's standard.",
-        website: 'http://google.com',
-        gitHub: 'http://github.com'),
+            "Create Flutter Package and Publish to the Pub.dev Beginner Friendly Steps",
+        website:
+            'https://gist.github.com/om-chauhan/4605653e6f2e41b8a0a46d6ab8e33b30',
+        gitHub:
+            'https://gist.github.com/om-chauhan/4605653e6f2e41b8a0a46d6ab8e33b30'),
     ProjectModel(
-        title: 'Project Name',
+        title: 'Flat Banners',
         description:
-            "is simply dummy text of the printing and tyesetting industry. Lorem Ipsum has been the industry's standard.",
-        website: 'http://google.com',
-        gitHub: 'http://github.com'),
+            "A Flutter package for Offers, Promo and Invites banners. Supports Android and Web.",
+        website: 'https://pub.dev/packages/flat_banners',
+        gitHub: 'https://github.com/om-chauhan/flat_banners'),
     ProjectModel(
-        title: 'Project Name',
+        title: 'Image Slider Indicator',
         description:
-            "is simply dummy text of the printing and tyesetting industry. Lorem Ipsum has been the industry's standard.",
-        website: 'http://google.com',
-        gitHub: 'http://github.com'),
+            "Image Slider with indicator using flutter framework and dart language.",
+        website: 'https://github.com/om-chauhan/Image-Slider-Indicator-Flutter',
+        gitHub: 'https://github.com/om-chauhan/Image-Slider-Indicator-Flutter'),
     ProjectModel(
-        title: 'Project Name',
+        title: 'Invoice Management Application UI',
         description:
-            "is simply dummy text of the printing and tyesetting industry. Lorem Ipsum has been the industry's standard.",
-        website: 'http://google.com',
-        gitHub: 'http://github.com'),
+            "Invoice Management Application Using Dart language and Flutter Framework Clean UI.",
+        website:
+            'https://github.com/om-chauhan/Invoice-Management-System-Flutter-UI',
+        gitHub:
+            'https://github.com/om-chauhan/Invoice-Management-System-Flutter-UI'),
     ProjectModel(
-        title: 'Project Name',
+        title: 'Ecommerce App',
+        description: "Ecommerce App - fetch data from Rest API.",
+        website:
+            'https://github.com/om-chauhan/Super-Store-Ecommerce-App-using-REST-Api-in-Flutter',
+        gitHub:
+            'https://github.com/om-chauhan/Super-Store-Ecommerce-App-using-REST-Api-in-Flutter'),
+    ProjectModel(
+        title: 'Simple Calculator',
         description:
-            "is simply dummy text of the printing and tyesetting industry. Lorem Ipsum has been the industry's standard.",
-        website: 'http://google.com',
-        gitHub: 'http://github.com'),
+            "A simple Calculator Appliction by using Dart Language and Flutter Framework. which is support both Ios and Android.",
+        website: 'https://github.com/om-chauhan/Simple-Calculator-Flutter',
+        gitHub: 'https://github.com/om-chauhan/Simple-Calculator-Flutter'),
   ];
 }
